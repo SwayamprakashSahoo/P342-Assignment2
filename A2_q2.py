24c9609491cbc683d4bd2b38dff9047cc1ca038d
SwayamprakashSahoo <42139077+SwayamprakashSahoo@users.noreply.github.com>
2020-08-17 14:13:52 +0530

    Update A2_q2.py

--- A2_q2.py
+++ A2_q2.py
@@ -1,8 +1,8 @@
 #Program to create two vectors of type A=(a1,a2,a3) and B=(b1,b2,b3) with numbers of your choice
 #(but not random numbers) in the code itself and to find A+B and A.B (dot product).
 
-vectA = [2, -5, 7] 
-vectB = [8, 4, 9] 
+vectA = [3, 5, 8] 
+vectB = [7, -4, 9] 
 sum = []
 product=[]
 dotp=0
@@ -13,6 +13,27 @@ for num1,num2 in zip(vectA,vectB):
 for num in range(3): 
     dotp = dotp + product[num]
 
-print("The vectors include:\nVector A :", vectA,"\nVector B :", vectB)
-print("\nThe sum of the two vectors is :", sum)
+print("The vectors include:\nVector A :")
+print("[",*vectA,"]")
+print("\nVector B :")
+print("[",*vectB,"]")
+print("\nThe sum of the two vectors is :")
+print("[",*sum,"]")
 print("\nThe dot product is :", dotp,"\n")
+
+
+
+
+########OUTPUT#######
+#The vectors include:
+#Vector A :
+#[ 3 5 8 ]
+#
+#Vector B :
+#[ 7 -4 9 ]
+#
+#The sum of the two vectors is :
+#[ 10 1 17 ]
+#
+#The dot product is : 73
+#
